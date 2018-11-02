c214a7d3c7c83b7f97ca5ae2e0bbf9edecd7f7ab
shellhub <shellhub.me@gmail.com>
2018-11-03 06:29:54 +0800

    format duration

--- app/src/main/java/shellhub/github/neteasemusic/util/MusicUtils.java
+++ app/src/main/java/shellhub/github/neteasemusic/util/MusicUtils.java
@@ -17,4 +17,34 @@ public class MusicUtils {
 
         return count;
     }
+
+    public static String formatDuration(long duration) {
+        long hours = duration / ConstantUtils.ONE_HOUR;
+        long minutes = (duration - hours * ConstantUtils.ONE_HOUR) / ConstantUtils.ONE_MINUTE;
+        long seconds = (duration - hours * ConstantUtils.ONE_HOUR - minutes * ConstantUtils.ONE_MINUTE) / ConstantUtils.ONE_SECOND;
+
+        StringBuilder builder = new StringBuilder();
+        if (hours != 0) {
+            if (hours < 10) {
+                builder.append("0").append(hours);
+            }else {
+                builder.append(hours);
+                builder.append(hours);
+            }
+            builder.append(":");
+        }
+        if (minutes < 10) {
+            builder.append("0").append(minutes);
+        }else {
+            builder.append(minutes);
+        }
+        builder.append(":");
+
+        if (seconds < 10) {
+            builder.append("0").append(seconds);
+        }else {
+            builder.append(seconds);
+        }
+        return builder.toString();
+    }
 }
---
+++ app/src/test/java/shellhub/github/neteasemusic/util/MusicUtilsTest.java
@@ -0,0 +1,17 @@
+package shellhub.github.neteasemusic.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MusicUtilsTest {
+
+    @Test
+    public void formatDuration() {
+        System.out.println(MusicUtils.formatDuration(5 * 60 * 1000));;
+        System.out.println(MusicUtils.formatDuration(3 * 60 * 1000 + 40 * 1000));
+        System.out.println(MusicUtils.formatDuration(3 * 60 * 1000 + 4 * 1000));
+        System.out.println(MusicUtils.formatDuration(1 * 60 * 60 * 1000 + 3 * 60 * 1000 + 40 * 1000));
+    }
+
+}
