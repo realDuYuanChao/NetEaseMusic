f00f0e6f8e98a8eeb2e6097573d5a17fd76788d9
shellhub <shellhub.me@gmail.com>
2018-11-06 19:49:12 +0800

    fix status bar bg

---
+++ app/src/main/java/shellhub/github/neteasemusic/ui/fragments/MainFragment.java
@@ -0,0 +1,32 @@
+package shellhub.github.neteasemusic.ui.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import shellhub.github.neteasemusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
