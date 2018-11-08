8ac8a59325e86acd34b2c52c58733f850d797abe
shellhub <shellhub.me@gmail.com>
2018-11-08 15:23:31 +0800

    remove useless code

--- app/src/main/java/shellhub/github/neteasemusic/ui/fragments/SearchFragment.java
+++ app/src/main/java/shellhub/github/neteasemusic/ui/fragments/SearchFragment.java
@@ -8,14 +8,11 @@ import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
 
-import org.greenrobot.eventbus.EventBus;
-
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import shellhub.github.neteasemusic.R;
-import shellhub.github.neteasemusic.adapter.LocalCategoryPagerAdapter;
 import shellhub.github.neteasemusic.adapter.SearchCategoryAdapter;
 
 /**
@@ -37,9 +34,6 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-//        if (!EventBus.getDefault().isRegistered(this)) {
-//            EventBus.getDefault().register(this);
-//        }
         ButterKnife.bind(this, view);
 
         setup();
@@ -51,6 +45,4 @@ public class SearchFragment extends Fragment {
         vpSearchCategory.setAdapter(new SearchCategoryAdapter(getFragmentManager()));
         tbSearchCategory.setupWithViewPager(vpSearchCategory);
     }
-
-
 }
